c21d7c0950f50cd314fe3f4745bf3ef6dcb14b4b
Briky <draconyth@gmail.com>
2024-08-31 21:34:20 -0700

    AttackManager: -implemented passives that affect advantage and damage in battle -currently checking tile conditions is working -TODO: check distance and direction conditions

--- ScriptableObjects/Battle/AttackManager.cs
+++ ScriptableObjects/Battle/AttackManager.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AttackManager", menuName = "ScriptableObjects/AttackManager", order = 1)]
 public class AttackManager : ScriptableObject
 {
+    public PassiveSkill passive;
+    public StatDatabase passiveData;
+    public int baseMultiplier;
+    protected int advantage;
+    protected int damageMultiplier;
     public void ActorAttacksActor(TacticActor attacker, TacticActor defender, BattleMap map, MoveCostManager moveManager)
     {
-        int advantage = 0;
-        // Determine advantage based on passives/etc.
+        advantage = 0;
+        damageMultiplier = baseMultiplier;
+        CheckPassives(attacker.allStats.attackingPassives, defender, attacker, map, moveManager);
+        CheckPassives(defender.allStats.defendingPassives, defender, attacker, map, moveManager);
         int damage = attacker.allStats.GetAttack();
+        Debug.Log(advantage);
+        Debug.Log(damageMultiplier);
         damage = Advantage(damage, advantage);
+        damage = damageMultiplier * damage / baseMultiplier;
         // Adjust damage based on passives, terrain effects, direction, etc.
         damage = Mathf.Max(0, damage - defender.allStats.GetDefense());
+        // Check if the passive affects damage.
         defender.allStats.UpdateHealth(damage);
         Debug.Log(defender.GetSpriteName()+" takes "+damage+" damage.");
         attacker.PayAttackCost();
@@ -45,5 +56,25 @@ public class AttackManager : ScriptableObject
             damage = Mathf.Min(damage, RollAttackDamage(baseAttack));
         }
         return damage - disadvantage;
+    }
+
+    protected void CheckPassives(List<string> characterPassives, TacticActor target, TacticActor other, BattleMap map, MoveCostManager moveManager)
+    {
+        for (int i = 0; i < characterPassives.Count; i++)
+        {
+            List<string> passiveStats = passiveData.ReturnStats(characterPassives[i]);
+            if (passive.CheckBattleCondition(passiveStats[1], passiveStats[2], target, other, map, moveManager))
+            {
+                switch (passiveStats[3])
+                {
+                    case "Advantage":
+                    advantage = passive.AffectInt(advantage, passiveStats[4], passiveStats[5]);
+                    break;
+                    case "Damage":
+                    damageMultiplier = passive.AffectInt(damageMultiplier, passiveStats[4], passiveStats[5]);
+                    break;
+                }
+            }
+        }
     }
 }
